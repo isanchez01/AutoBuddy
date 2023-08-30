37cdb8bd26127d3727077ef72013eb580c16ac1a
Israel Sanchez <99135341+isanchez01@users.noreply.github.com>
2023-08-30 14:47:48 -0500

    Setup credentials

--- ab/settings.py
+++ ab/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-dd#zuil!@v@%&+uebfvss_&soll+c=+h-68zm^@8rp@*dv4aw=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -75,8 +74,12 @@ WSGI_APPLICATION = 'ab.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '0.0.0.0',
+        'PORT': '5432',
     }
 }
 
