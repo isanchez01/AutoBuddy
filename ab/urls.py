653854797b4316d22502e43786faf5251adfd0da
Israel Sanchez <99135341+isanchez01@users.noreply.github.com>
2023-08-31 09:47:24 -0500

    Completion of web setup

--- ab/urls.py
+++ ab/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from autobuddy import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.index),
 ]
