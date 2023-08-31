240e88f2588b36a0d8fc6af9c465824b7df67f49
Israel Sanchez <99135341+isanchez01@users.noreply.github.com>
2023-08-30 20:06:09 -0500

    Modeling

---
+++ autobuddy/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class RepairCost(models.Model):
+    car_year = models.CharField(max_length=4)
+    car_make = models.CharField(max_length=30)
+    car_model = models.CharField(max_length=60)
+    
+    repair_name = models.CharField(max_length = 70)
+    repair_cost = models.IntegerField(blank= True)
+    repair_quote_cost = models.IntegerField(blank=True)
+
+    def __str__(self) -> str:
+        return self.car_year + " " + self.car_make + " " + self.car_model + " " + self.repair_name + " $" + str(self.repair_cost)
